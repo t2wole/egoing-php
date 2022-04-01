1a274878b90c27837ff870b12b712a4faa8a044b
t2wole <t2wole@gmail.com>
2022-04-01 13:50:42 +0900

    second

---
+++ number.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<body>    
+    <h1>1+1</h1>
+    <?php
+    echo 1+1;
+    ?>
+    <h1>1*1</h1>
+    <?php
+    echo 1*1;
+    ?>
+</body>
+</html>
+
---
+++ string.php
@@ -0,0 +1,12 @@
+<?php
+echo 'hello world';
+?>
+
+<?php
+echo 'hello '.'world';
+?>
+
+<?php
+echo strlen("hello world");
+?>
+
